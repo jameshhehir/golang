c8dfc6b831b6c368c54475bdb4d59a24a34db164
Jimmy Hehir <jameshhehir@gmail.com>
2020-02-06 15:48:42 +0000

    initial commit

---
+++ helloWorld.go
@@ -0,0 +1,7 @@
+package main // starting point of the program
+
+import "fmt" // fmt package is used to implement formatted input/output with fucntions
+
+func main() {
+	fmt.Println("Hello World")
+}
